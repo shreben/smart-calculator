95f402cdb6403f050a82215bbc452c751c5825e1
Siarhei Hreben <sergei.greben@gmail.com>
2018-11-09 17:01:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 class SmartCalculator {
   constructor(initialValue) {
+    this.initialValue = initialValue;
     // your implementation
   }
 
   add(number) {
+    return this.initialValue + number;
     // your implementation
   }
   
   subtract(number) {
+    return this.initialValue - number;
     // your implementation
   }
 
   multiply(number) {
+    return this.initialValue * number;
     // your implementation
   }
 
   devide(number) {
+    return this.initialValue / number;
     // your implementation
   }
 
   pow(number) {
+    return Math.pow(this.initialValue, number);
     // your implementation
   }
 }
